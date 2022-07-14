d7cf49ccdd327632102e673dd0dd3e0c99f60504
五道杠 <wangdiangang@qinjia001.com>
2022-07-14 14:01:20 +0800

    部署的node服务器

--- src/view/other.vue
+++ src/view/other.vue
@@ -3,12 +3,15 @@
     <van-button @click="goOther">去往其他1页面</van-button>
     <van-button @click="goSke">去往骨架屏页面</van-button>
     <van-button type="success" @click="get">接口</van-button>
+    <van-button type="success" @click="getAli">阿里服务器接口</van-button>
     <van-button type="success" @click="lucky">去抽奖</van-button>
   </div>
 </template>
 <script>
 import { useRouter } from "vue-router";
 import { getCode } from "../api/api.js";
+import axios from "axios"
+import { Toast } from 'vant';
 export default {
   setup() {
     const router = useRouter();
@@ -20,6 +23,7 @@ export default {
     };
     const get = () => {
       console.log("get");
+
       let data = {
         codeCount: "4",
         height: 100,
@@ -29,8 +33,15 @@ export default {
       };
       getCode(data).then((res) => {
         console.log(res);
+       
       });
     };
+    const getAli=()=>{
+      axios.get('http://39.105.40.151:8080').then(res=>{
+        console.log(res);
+         Toast('获取node服务器接口成功'+res.request.responseText)
+      })
+    }
     const lucky=()=>{
       router.push('/lucky')
     }
@@ -38,7 +49,8 @@ export default {
       goOther,
       goSke,
       get,
-      lucky
+      lucky,
+      getAli
     };
   },
 };
